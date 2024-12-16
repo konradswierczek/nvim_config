6794b9728b7bc47ff2e79734f748d9b8eba3cfa7
konradswierczek <swierckj@mcmaster.ca>
2024-12-16 14:08:50 +0000

    feat(plugins): Add colourshceme and neotree, and some key mappings

--- init.lua
+++ init.lua
@@ -5,3 +5,12 @@ vim.cmd [[
   autocmd FileType python highlight ColorColumn ctermbg=red guibg=red
 ]]
 
+-- Define the key mapping function
+function map(mode, lhs, rhs)
+    vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
+end
+
+-- NeoTree Key Mappings
+map("n", "<leader>e", "<CMD>Neotree toggle<CR>")
+map("n", "<leader>r", "<CMD>Neotree focus<CR>")
+
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,14 @@
+-- ~/nvim/lua/plugins/neotree.lua
+
+-- Plugin configuration
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+}
+
